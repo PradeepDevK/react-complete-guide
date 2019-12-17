ce6ca0d3cd1f13b523c4589037b50a55310c48c2
ACCIONLABS\Al1430 <pradeepjack555@gmail.com>
2019-12-17 15:02:49 +0530

    props and state

--- src/App.js
+++ src/App.js
@@ -16,16 +16,36 @@ const app = props => {
 
   console.log(personsState, otherState);
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
+    // alert('Was Clicked!');
+    // Do Not Do This: this.state.person[0].name = "Pradeep";
+    setPersonsState({
+      person: [
+        { name: newName, age: 29 },
+        { name: 'Pratheep', age: 27 },
+        { name: 'Pradip', age: 28 }
+      ]
+    });
+  };
+
+  const nameChangedHandler = (event) => {
     // alert('Was Clicked!');
     // Do Not Do This: this.state.person[0].name = "Pradeep";
     setPersonsState({
       person: [
         { name: 'Pradeep', age: 29 },
-        { name: 'Pratheep', age: 27 },
+        { name: event.target.value, age: 27 },
         { name: 'Pradip', age: 28 }
       ]
     });
+  };
+
+  const style = {
+    backgroudColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
   };
 
 
@@ -33,10 +53,19 @@ const app = props => {
     <div className="App">
       <h1>Hi, I'm a react app.</h1>
       <p>This is really working..</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.person[0].name} age={personsState.person[0].age} />
-      <Person name={personsState.person[1].name} age={personsState.person[1].age}>My Hobbies : Racing </Person>
-      <Person name={personsState.person[2].name} age={personsState.person[2].age} />
+      <button
+        style = {style}
+        onClick={() => switchNameHandler("Pradeep!")}>Switch Name</button>
+      <Person
+        name={personsState.person[0].name}
+        age={personsState.person[0].age} />
+      <Person
+        name={personsState.person[1].name}
+        age={personsState.person[1].age} click={switchNameHandler.bind(this, "PradeepRaj")}
+        changed={nameChangedHandler}>My Hobbies : Racing </Person>
+      <Person
+        name={personsState.person[2].name}
+        age={personsState.person[2].age} />
     </div>
   );
 
